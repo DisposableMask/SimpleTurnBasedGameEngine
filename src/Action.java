27ad3d59f9cc9c138d27b6ac6f777a4cf222fd00
DisposableMask <anexero@outlook.com>
2026-01-07 16:29:40 +0800

    Create abstract BaseAction class implementing action interface

--- src/Action.java
+++ src/Action.java
@@ -2,5 +2,21 @@
 @FunctionalInterface
 public interface Action
 {
-    void Perform();
+    // All Actions should be logged and be capable of being performed
+    void Perform(String ActionName);
+}
+
+
+abstract class BaseAction implements Action
+{
+    // Final to prevent child classes to override
+    @Override
+    public final void Perform(String ActionName)
+    {
+        System.out.println("Currently performing " + ActionName + "...");
+        Execution();
+    }
+
+    // Child classes to implement its own functionality
+    protected abstract void Execution();
 }
