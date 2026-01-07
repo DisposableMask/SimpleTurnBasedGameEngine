1f9c4f43831482c137eb2548c877951a44674bfc
DisposableMask <anexero@outlook.com>
2026-01-07 19:34:35 +0800

    Added available actions to location class

--- src/locations/Location.java
+++ src/locations/Location.java
@@ -1,6 +1,12 @@
 package locations;
 
+import actions.BaseAction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Location
 {
-
+    // Available actions at the location
+    private List<BaseAction> availableActions = new ArrayList<>();
 }
